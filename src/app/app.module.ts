3d52bb5e83b5e106cebb2831d6ab498e5ae91101
jamieafriedt <35972904+jamieafriedt@users.noreply.github.com>
2018-03-20 19:25:06 -0500

    src
    src folder for base app

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AssessmentComponent } from './assessment/assessment.component';
+import { AssessmentResultsComponent } from './assessment-results/assessment-results.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {routing} from './app.routing';
+import { FormsModule }    from '@angular/forms';
+import { ManagerDashboardComponent } from './manager-dashboard/manager-dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AssessmentComponent,
+    AssessmentResultsComponent,
+    UserDashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    ManagerDashboardComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
