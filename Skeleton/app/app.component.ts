21e4240dda4376098c2a73125e9222c4bd4b4e2e
vause@yahoo.com <vause@yahoo.com>
2018-03-24 12:05:13 -0500

    Commit of js project
    Only Client side at the moment. Server side will be pushed soon

--- Skeleton/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    moduleId: module.id.toString(),
-    selector: 'app',
-    templateUrl: 'app.component.html'
-})
-
-export class AppComponent { }
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('client App', () => {
+describe('ten-key-app App', () => {
   let page: AppPage;
 
   beforeEach(() => {
